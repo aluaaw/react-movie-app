c1e2547f38180e7f4ba0dee862d02f9bb20e852e
aluaaw <ylua.aw@gmail.com>
2021-04-13 21:24:19 +0900

    react life cycle methods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 class App extends React.Component {
+
     state = {
         count: 0
     }
@@ -11,7 +12,22 @@ class App extends React.Component {
         this.setState(current => ({count: current.count - 1}))
     }
 
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+    componentDidMount() {
+        console.log('mount');
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('update');
+    }
+    componentWillUnmount() {
+        console.log('good bye');
+    }
+
     render() {
+        console.log('rendering')
         return <div>
             <h1>the number is {this.state.count}</h1>
             <button id="plus" onClick={this.plus}>plus</button>
